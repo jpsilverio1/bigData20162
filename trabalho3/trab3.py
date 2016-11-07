1283eca850fb07cdb6a3dc47ee707355b4d7b8f0
Jéssica SIlvério <Jssica SIlvrio>
2016-11-07 16:39:31 -0500

    Updated version of the code. Has methods to normalize the datasby feature and save the normalized dataset to a csv file. Also has methods to identify some candidates for removal and save their index in an csv file

--- trabalho3/trab3.py
+++ trabalho3/trab3.py
@@ -1,26 +1,54 @@
-# Load the Pima Indians diabetes dataset from CSV URL
+
+from __future__ import division
 import numpy as np
-def allDifferent(list):
+from sklearn.preprocessing import normalize
+import csv
+
+def allEqual(list):
     return ( len(set(list)) == 1)
 def checkFeatures(list):
     cont =0
+    equalColumns = []
     for i in range(len(list[0])):
         col = list[:,i]
-        print col
-        if (allDifferent(col)):
+        if (allEqual(col)):
            print("all different {} ".format(i))
+           equalColumns.append(i)
            cont +=1
     print cont
-raw_data =  open("train_file.csv")
-raw_data.readline()  # skip the header
-# load the CSV file as a numpy matrix
-dataset = np.loadtxt(raw_data, delimiter=",")
+    return equalColumns
+
+def normalizeMatrixByColumns(matrix):
+    newMatrix = []
+    rowVector = []
+    for i in range(len(matrix[0])):
+        col = matrix[:,i]
+        maxValue = np.amax(col)
+        if (maxValue != 0):
+            for j in range(len(col)):
+                rowVector.append(col[j]/maxValue)
+        newMatrix.append(rowVector)
+        rowVector = []
+    return newMatrix
+def normalizeDatasetByColumns(dataset):
+	return normalize(dataset, axis = 0)
+def getDatasetFromCSV(fileName):
+	raw_data =  open(fileName)
+	header = raw_data.readline()  
+	print header
+	# load the CSV file as a numpy matrix
+	return np.loadtxt(raw_data, delimiter=",")
+def writeFileFromList(fileName,mylist):
+	myfile = open(fileName, 'wb')
+	wr = csv.writer(myfile)
+	wr.writerow(mylist)
+dataset = getDatasetFromCSV("train_file.csv")
 print(dataset.shape)
 print(dataset)
-checkFeatures(dataset)
-print len(dataset[0])
-# separate the data from the target attributes
-X = dataset[:,0:7]
-y = dataset[:,8]
-
+columns_To_Remove = checkFeatures(dataset)
+writeFileFromList("columnsToRemove.csv", columns_To_Remove);
+columns_normed = normalizeDatasetByColumns(dataset)
+#np.savetxt('normalizedTrainDataset.csv', columns_normed, delimiter=',')
+print "normalized"
+print (columns_normed)
 
